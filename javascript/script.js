df735a2b0224cc903c022d8a1b1de5010f5519a4
unknown <vinakmal554@gmail.com>
2024-02-13 14:07:05 +0700

    update version 1.3

--- javascript/script.js
+++ javascript/script.js
@@ -12,27 +12,14 @@ document.querySelector('#menu').onclick = () => {
         e.preventDefault();
     }
 
-// toogle seacrh
-const search = document.querySelector('.search')
-const searchBox = document.querySelector('#searchBox')
-
-document.querySelector('#searchButton').onclick = (e) => {
-    search.classList.toggle('active');
-    searchBox.focus();
-    e.preventDefault();
-};
 
 // bebas klik
 const menu = document.querySelector('#menu');
-const sb = document.querySelector('#searchButton');
 const kr = document.querySelector('#shoppingButton');
 
 document.addEventListener('click', function(e) {
     if(!menu.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
-    }
-    if(!sb.contains(e.target) && !search.contains(e.target)) {
-        search.classList.remove('active');
     }
     if(!kr.contains(e.target) && !shoppingCart.contains(e.target)) {
         shoppingCart.classList.remove('active');
